10b51cc8d0bf488d7780f2235437090593959b5e
Soumen Mahali <145513814+s-mahali@users.noreply.github.com>
2025-11-09 10:12:49 +0530

    Update axios baseURL for production environment

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const serviceAxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_NODE_ENV === "PROD" ? "https://google-calendar-agent-i68n.onrender.com" : "http://localhost:3600",
+    baseURL: import.meta.env.VITE_NODE_ENV === "PROD" ? "https://api.seedhecodestudio.tech" : "http://localhost:3600",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
